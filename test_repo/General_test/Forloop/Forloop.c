c0051dba314974e4f462e4ed1d5f7ced6c0fec44
Joseph kakai <josephngalu96@gmail.com>
2022-09-27 19:38:24 +0300

    Added task for

---
+++ test_repo/General_test/Forloop/Forloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints hello world and the oucome of for loop and a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (int i = 1; i < 11; i++)
+	{
+		printf("Hello, World!\n");
+	}
+	return (0);
+}
