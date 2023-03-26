aa25f2ce5d53d1d012a3911f590ffb0989958eb2
Wanton Raven <49359467+wantonraven@users.noreply.github.com>
2023-03-26 21:32:13 +0300

    Create convert_human_to_dog_years.js

---
+++ test_repo/convert_human_to_dog_years.js
@@ -0,0 +1,25 @@
+/* Declared a varibale and assigned human\
+ years */
+const myAge = 40;
+// Declared another variable and assigned to 2
+let earlyYears = 2;
+/* the variable 'earlyYears' is assigned to\
+ its multiplication by 10.5 */
+earlyYears *= 10.5;
+/* Declared another variable and assigned to\
+ the subraction of 'myAge' variable by */
+let laterYears = myAge - 2;
+/* multiplied 'later Years' by 4 and assigned\
+ to it */
+laterYears *= 4;
+// output to the console 'earlyYears' and 'laterYears' 
+console.log(`earlyYears: ${earlyYears} \nlaterYear: ${laterYears}`);
+/* Declared 'myAgeInDogYears' and stored sum\
+ of 'earlyYears' and 'laterYears'. */
+let myAgeInDogYears = earlyYears + laterYears;
+/* Declared 'myName' variable and assigned\
+ the string 'Wanton Raven' and converted to\
+  small letters. */
+myName = 'Wanton Raven'.toLowerCase();
+// printed the output to the console
+console.log(`My name is ${myName}. I ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
