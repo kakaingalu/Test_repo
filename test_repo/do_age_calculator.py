587fd8e5f70abd8d7bef3802fc3d4c84ca243a75
Wanton Raven <49359467+wantonraven@users.noreply.github.com>
2023-03-26 21:54:28 +0300

    Create do_age_calculator.py

---
+++ test_repo/do_age_calculator.py
@@ -0,0 +1,12 @@
+/* Declared 'myDogName' variable and assigned\
+ to 'Ron' */
+ const myDogName = 'Ron'.toUpperCase();
+/* Declared 'myname' variable and assigned to the string 'Wanton Raven'*/
+ const myName = 'Wanton Raven'.toUpperCase();
+//Declared 'myAge' variable assigned 40
+let myAge = 40;
+/* since human 1 year age is four times dog years we divide by 4 */
+myDogAge = myAge / 4;
+// printed the output to the console
+console.log(`My name is ${myName} my age is ${myAge} in human years, \
+I call my Dog ${myDogName}. He has ${myDogAge} years old in Dog years.`)
