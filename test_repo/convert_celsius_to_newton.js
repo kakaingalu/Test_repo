d87c0bcc7b8f15585e77b704a9c0ef1045b19643
Wanton Raven <49359467+wantonraven@users.noreply.github.com>
2023-03-26 20:31:48 +0300

    Create convert_celsius_to_newton.js

---
+++ test_repo/convert_celsius_to_newton.js
@@ -0,0 +1,8 @@
+// Declared a variable named celsius and assigned 30
+const celsius = 30;
+// And now conversion of of celsius to Newton 
+let Newton = celsius * (33/100);
+// round down fahrenheit from decimal
+Newton = Math.floor(Newton);
+
+console.log(`The temperature is ${Newton} degrees Newton.`)
